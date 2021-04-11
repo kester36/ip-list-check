958beef6bf8ccd32294b51546d3987b7f41c7715
Petro Shpyhotskyi <shpyhotskyi@gmail.com>
2021-04-11 19:34:20 +0300

    add methods to IpInterface

--- src/IpInterface.php
+++ src/IpInterface.php
@@ -6,5 +6,8 @@ namespace Kester36\IpCollection;
 
 interface IpInterface
 {
+    /** @throws InvalidArgumentException */
+    public function __construct(string $ip);
 
+    public function support(string $ip): bool;
 }
